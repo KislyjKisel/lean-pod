b2cdf7f29a3f48e8b8f38ce3d2dc4ca41470b60a
KislyjKisel <danilakisel@gmail.com>
2023-04-18 17:02:34 +0300

    `BytesRef`: fix ffi signatures, add mutability param

--- src/lean/Pod/BytesRef.lean
+++ src/lean/Pod/BytesRef.lean
@@ -1,161 +1,199 @@
+import Pod.Lemmas
+import Pod.Util
 import Pod.UInt
 
 set_option autoImplicit false
 
 namespace Pod
 
-opaque BytesRefPointed (σ : Type) (size : USize) (alignment : Nat) : NonemptyType
-def BytesRef (σ : Type) (size : USize) (alignment : Nat) : Type := (BytesRefPointed σ size alignment).type
-instance {σ size alignment} : Nonempty (BytesRef σ size alignment) := (BytesRefPointed σ size alignment).property
+inductive Mutability where
+  | Mutable
+  | Immutable
 
-@[extern "lean_pod_BytesView_id"]
-opaque weaken {σ size alignment0 alignment1} (h : alignment1 ≤ alignment0) :
-  BytesRef σ size alignment0 → BytesRef σ size alignment1
+variable {σ : Type}
 
-@[extern "lean_pod_BytesView_id"]
-opaque take {σ size alignment} (bv : BytesRef σ size alignment) (count : USize) (h : count ≤ size) : BytesRef σ count alignment
+opaque BytesRefPointed (σ : Type) (mutab : Mutability) (size : USize) (align : Nat) : NonemptyType
 
-@[extern "lean_pod_BytesView_drop"]
-opaque drop {σ size alignment} (bv : BytesRef σ size alignment) (count : USize) (h : count ≤ size) : BytesRef σ (size - count) (alignment.gcd count.toNat)
+def BytesRef (σ : Type) (mutab : Mutability) (size : USize) (align : Nat) : Type :=
+  (BytesRefPointed σ mutab size align).type
+
+abbrev BytesRefMut (σ : Type) := BytesRef σ .Mutable
+abbrev BytesRefImm (σ : Type) := BytesRef σ .Immutable
+
+instance {size mutab align} : Nonempty (BytesRef σ mutab size align) := (BytesRefPointed σ mutab size align).property
+
+namespace BytesRef
+
+@[extern "lean_pod_BytesRef_drop"]
+opaque weaken {mutab size} {align0 align1 : @& Nat} (h : align1 ≤ align0) :
+  BytesRef σ mutab size align0 → BytesRef σ mutab size align1
+
+@[extern "lean_pod_BytesRef_take"]
+opaque take {mutab size} {align : @& Nat} (bv : BytesRef σ mutab size align)
+  (count : USize) (h : count ≤ size) : BytesRef σ mutab count align
+
+@[extern "lean_pod_BytesRef_drop"]
+opaque drop {mutab size} {align : @& Nat} (bv : BytesRef σ mutab size align)
+  (count : USize) (h : count ≤ size) : BytesRef σ mutab (size - count) (align.gcd count.toNat)
+
+@[extern "lean_pod_BytesRef_slice"]
+opaque slice {mutab size} {align : @& Nat} (bv : BytesRef σ mutab size align) (start length : USize)
+  (bounded : start.toNat + length.toNat ≤ size.toNat) : BytesRef σ mutab length (align.gcd start.toNat) :=
+  let «start≤size» : start ≤ size := by
+    apply Nat.le_trans $ Nat.le_sub_of_add_le bounded
+    exact Nat.sub_le size.toNat length.toNat
+  (bv.drop start «start≤size»).take length $ by
+    show length.val ≤ (size.val - start.val).val
+    rw [Fin.toNat_sub_distrib size.val start.val «start≤size»]
+    apply Nat.le_sub_of_add_le
+    rw [Nat.add_comm]
+    exact bounded
+
+/--
+Read all bytes into a `ByteArray`.
+**Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
+-/
+@[extern "lean_pod_BytesRef_toByteArray"]
+opaque toByteArray {mutab size} {align : @& Nat} (bv : @& BytesRef σ mutab size align) : ST σ ByteArray
 
 /--
 Read a `UInt8`.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_getUInt8"]
-opaque getUInt8 {σ size alignment} (bv : @& BytesRef σ size alignment) (i : USize) (h : i < size) : ST σ UInt8
+opaque getUInt8 {mutab size} {align : @& Nat} (bv : @& BytesRef σ mutab size align) (i : USize) (h : i < size) : ST σ UInt8
 
 /--
 Read a `UInt16` stored in native-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_getUInt16Ne"]
-opaque getUInt16Ne {σ size} (bv : @& BytesRef σ size UInt16.alignment) (i : USize) (h : i + 1 < size) : ST σ UInt16
+opaque getUInt16Ne {mutab size} (bv : @& BytesRef σ mutab size UInt16.alignment) (i : USize) (h : i + 1 < size) : ST σ UInt16
 
 /--
 Read a `UInt16` stored in little-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_getUInt16Le"]
-opaque getUInt16Le {σ size} (bv : @& BytesRef σ size UInt16.alignment) (i : USize) (h : i + 1 < size) : ST σ UInt16
+opaque getUInt16Le {mutab size} (bv : @& BytesRef σ mutab size UInt16.alignment) (i : USize) (h : i + 1 < size) : ST σ UInt16
 
 /--
 Read a `UInt16` stored in big-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_getUInt16Be"]
-opaque getUInt16Be {σ size} (bv : @& BytesRef σ size UInt16.alignment) (i : USize) (h : i + 1 < size) : ST σ UInt16
+opaque getUInt16Be {mutab size} (bv : @& BytesRef σ mutab size UInt16.alignment) (i : USize) (h : i + 1 < size) : ST σ UInt16
 
 /--
 Read a `UInt32` stored in native-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_getUInt32Ne"]
-opaque getUInt32Ne {σ size} (bv : @& BytesRef σ size UInt32.alignment) (i : USize) (h : i + 3 < size) : ST σ UInt32
+opaque getUInt32Ne {mutab size} (bv : @& BytesRef σ mutab size UInt32.alignment) (i : USize) (h : i + 3 < size) : ST σ UInt32
 
 /--
 Read a `UInt32` stored in little-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_getUInt32Le"]
-opaque getUInt32Le {σ size} (bv : @& BytesRef σ size UInt32.alignment) (i : USize) (h : i + 3 < size) : ST σ UInt32
+opaque getUInt32Le {mutab size} (bv : @& BytesRef σ mutab size UInt32.alignment) (i : USize) (h : i + 3 < size) : ST σ UInt32
 
 /--
 Read a `UInt32` stored in big-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_getUInt32Be"]
-opaque getUInt32Be {σ size} (bv : @& BytesRef σ size UInt32.alignment) (i : USize) (h : i + 3 < size) : ST σ UInt32
+opaque getUInt32Be {mutab size} (bv : @& BytesRef σ mutab size UInt32.alignment) (i : USize) (h : i + 3 < size) : ST σ UInt32
 
 /--
 Read a `UInt64` stored in native-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_getUInt64Ne"]
-opaque getUInt64Ne {σ size} (bv : @& BytesRef σ size UInt64.alignment) (i : USize) (h : i + 7 < size) : ST σ UInt64
+opaque getUInt64Ne {mutab size} (bv : @& BytesRef σ mutab size UInt64.alignment) (i : USize) (h : i + 7 < size) : ST σ UInt64
 
 /--
 Read a `UInt64` stored in little-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_getUInt64Le"]
-opaque getUInt64Le {σ size} (bv : @& BytesRef σ size UInt64.alignment) (i : USize) (h : i + 7 < size) : ST σ UInt64
+opaque getUInt64Le {mutab size} (bv : @& BytesRef σ mutab size UInt64.alignment) (i : USize) (h : i + 7 < size) : ST σ UInt64
 
 /--
 Read a `UInt64` stored in big-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_getUInt64Be"]
-opaque getUInt64Be {σ size} (bv : @& BytesRef σ size UInt64.alignment) (i : USize) (h : i + 7 < size) : ST σ UInt64
+opaque getUInt64Be {mutab size} (bv : @& BytesRef σ mutab size UInt64.alignment) (i : USize) (h : i + 7 < size) : ST σ UInt64
 
 /--
 Write to a `UInt8`.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_setUInt8"]
-opaque setUInt8 {σ size alignment} (bv : @& BytesRef σ size alignment) (i : USize) (h : i < size) (value : UInt8) : ST σ Unit
+opaque setUInt8 {σ size align} (bv : @& BytesRefMut σ size align) (i : USize) (h : i < size) (value : UInt8) : ST σ Unit
 
 /--
 Write to a `UInt16` stored in native-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_setUInt16Ne"]
-opaque setUInt16Ne {σ size} (bv : @& BytesRef σ size UInt16.alignment) (i : USize) (h : i + 1 < size) (value : UInt16) : ST σ Unit
+opaque setUInt16Ne {size} (bv : @& BytesRefMut σ size UInt16.alignment) (i : USize) (h : i + 1 < size) (value : UInt16) : ST σ Unit
 
 /--
 Write to a `UInt16` stored in little-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_setUInt16Le"]
-opaque setUInt16Le {σ size} (bv : @& BytesRef σ size UInt16.alignment) (i : USize) (h : i + 1 < size) (value : UInt16) : ST σ Unit
+opaque setUInt16Le {size} (bv : @& BytesRefMut σ size UInt16.alignment) (i : USize) (h : i + 1 < size) (value : UInt16) : ST σ Unit
 
 /--
 Write to a `UInt16` stored in big-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_setUInt16Be"]
-opaque setUInt16Be {σ size} (bv : @& BytesRef σ size UInt16.alignment) (i : USize) (h : i + 1 < size) (value : UInt16) : ST σ Unit
+opaque setUInt16Be {size} (bv : @& BytesRefMut σ size UInt16.alignment) (i : USize) (h : i + 1 < size) (value : UInt16) : ST σ Unit
 
 /--
 Write to a `UInt32` stored in native-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_setUInt32Ne"]
-opaque setUInt32Ne {σ size} (bv : @& BytesRef σ size UInt32.alignment) (i : USize) (h : i + 3 < size) (value : UInt32) : ST σ Unit
+opaque setUInt32Ne {size} (bv : @& BytesRefMut σ size UInt32.alignment) (i : USize) (h : i + 3 < size) (value : UInt32) : ST σ Unit
 
 /--
 Write to a `UInt32` stored in little-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_setUInt32Le"]
-opaque setUInt32Le {σ size} (bv : @& BytesRef σ size UInt32.alignment) (i : USize) (h : i + 3 < size) (value : UInt32) : ST σ Unit
+opaque setUInt32Le {size} (bv : @& BytesRefMut σ size UInt32.alignment) (i : USize) (h : i + 3 < size) (value : UInt32) : ST σ Unit
 
 /--
 Write to a `UInt32` stored in big-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_setUInt32Be"]
-opaque setUInt32Be {σ size} (bv : @& BytesRef σ size UInt32.alignment) (i : USize) (h : i + 3 < size) (value : UInt32) : ST σ Unit
+opaque setUInt32Be {size} (bv : @& BytesRefMut σ size UInt32.alignment) (i : USize) (h : i + 3 < size) (value : UInt32) : ST σ Unit
 
 /--
 Write to a `UInt64` stored in native-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_setUInt64Ne"]
-opaque setUInt64Ne {σ size} (bv : @& BytesRef σ size UInt64.alignment) (i : USize) (h : i + 7 < size) (value : UInt64) : ST σ Unit
+opaque setUInt64Ne {size} (bv : @& BytesRefMut σ size UInt64.alignment) (i : USize) (h : i + 7 < size) (value : UInt64) : ST σ Unit
 
 /--
 Write to a `UInt64` stored in little-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_setUInt64Le"]
-opaque setUInt64Le {σ size} (bv : @& BytesRef σ size UInt64.alignment) (i : USize) (h : i + 7 < size) (value : UInt64) : ST σ Unit
+opaque setUInt64Le {size} (bv : @& BytesRefMut σ size UInt64.alignment) (i : USize) (h : i + 7 < size) (value : UInt64) : ST σ Unit
 
 /--
 Write to a `UInt64` stored in big-endian order.
 **Panics** when used in multithreaded context or if the `ByteRef` was marked persistent.
 -/
 @[extern "lean_pod_BytesRef_setUInt64Be"]
-opaque setUInt64Be {σ size} (bv : @& BytesRef σ size UInt64.alignment) (i : USize) (h : i + 7 < size) (value : UInt64) : ST σ Unit
+opaque setUInt64Be {size} (bv : @& BytesRefMut σ size UInt64.alignment) (i : USize) (h : i + 7 < size) (value : UInt64) : ST σ Unit
 
-end Pod
+end Pod.BytesRef
--- src/native/bytes_ref.c
+++ src/native/bytes_ref.c
@@ -2,6 +2,27 @@
 #include "include/lean_pod.h"
 #include "util.h"
 
+LEAN_EXPORT lean_obj_res lean_pod_BytesRef_weaken(b_lean_obj_arg mut, size_t sz, b_lean_obj_arg a0, b_lean_obj_arg a1, lean_obj_arg bv) {
+    return bv;
+}
+
+LEAN_EXPORT lean_obj_res lean_pod_BytesRef_take(b_lean_obj_arg mut, size_t size, b_lean_obj_arg a, lean_obj_arg bv, size_t count) {
+    return bv;
+}
+
+LEAN_EXPORT lean_obj_res lean_pod_BytesRef_drop(b_lean_obj_arg mut, size_t size, b_lean_obj_arg a, lean_obj_arg bv_w, size_t count) {
+    lean_pod_BytesView* bv = lean_pod_BytesView_unwrap(bv_w);
+    if(lean_is_exclusive(bv_w)) {
+        bv->ptr += count;
+        return bv_w;
+    }
+    return lean_pod_BytesView_wrap(bv->ptr + count, bv->owner);
+}
+
+LEAN_EXPORT lean_obj_res lean_pod_BytesRef_slice(b_lean_obj_arg mut, size_t sz, b_lean_obj_arg a, lean_obj_arg bv_w, size_t start, size_t length) {
+    return lean_pod_BytesRef_drop(mut, sz, a, bv_w, start);
+}
+
 static inline lean_obj_res lean_pod_BytesRef_notStError() {
     return lean_io_result_mk_ok(lean_panic_fn(
         lean_box(0),
@@ -9,14 +30,20 @@ static inline lean_obj_res lean_pod_BytesRef_notStError() {
     ));
 }
 
-LEAN_EXPORT lean_obj_res lean_pod_BytesRef_getUInt8(b_lean_obj_arg br, size_t i) {
+LEAN_EXPORT lean_obj_res lean_pod_BytesRef_toByteArray(b_lean_obj_arg mut, size_t sz, b_lean_obj_arg a, b_lean_obj_arg br) {
+    lean_object* arr = lean_alloc_sarray(1, sz, sz);
+    memcpy(lean_sarray_cptr(arr), lean_pod_BytesView_unwrap(br)->ptr, sz);
+    return arr;
+}
+
+LEAN_EXPORT lean_obj_res lean_pod_BytesRef_getUInt8(b_lean_obj_arg mut, size_t sz, b_lean_obj_arg a, b_lean_obj_arg br, size_t i) {
     if (LEAN_LIKELY(lean_is_st(br))) {
         return lean_io_result_mk_ok(lean_pod_BytesView_unwrap(br)->ptr[i]);
     }
     return lean_pod_BytesRef_notStError();
 }
 
-LEAN_EXPORT lean_obj_res lean_pod_BytesRef_setUInt8(b_lean_obj_arg br, size_t i, uint8_t value) {
+LEAN_EXPORT lean_obj_res lean_pod_BytesRef_setUInt8(size_t sz, b_lean_obj_arg a, b_lean_obj_arg br, size_t i, uint8_t value) {
     if (LEAN_LIKELY(lean_is_st(br))) {
         lean_pod_BytesView_unwrap(br)->ptr[i] = value;
         return lean_io_result_mk_ok(lean_box(0));
@@ -25,7 +52,7 @@ LEAN_EXPORT lean_obj_res lean_pod_BytesRef_setUInt8(b_lean_obj_arg br, size_t i,
 }
 
 #define LEAN_POD_BYTESREF_GET_SET(bw, box)\
-LEAN_EXPORT lean_obj_res lean_pod_BytesRef_getUInt##bw##Ne(b_lean_obj_arg br, size_t i) {\
+LEAN_EXPORT lean_obj_res lean_pod_BytesRef_getUInt##bw##Ne(b_lean_obj_arg mut, size_t sz, b_lean_obj_arg br, size_t i) {\
     if (LEAN_LIKELY(lean_is_st(br))) {\
         uint##bw##_t u;\
         memcpy(&u, lean_pod_BytesView_unwrap(br)->ptr + i, sizeof(uint##bw##_t));\
@@ -33,10 +60,10 @@ LEAN_EXPORT lean_obj_res lean_pod_BytesRef_getUInt##bw##Ne(b_lean_obj_arg br, si
     }\
     return lean_pod_BytesRef_notStError();\
 }\
-LEAN_EXPORT lean_obj_res LEAN_POD_CONCAT3(lean_pod_BytesRef_getUInt, bw, LEAN_POD_BYTESREF_E_KEEP)(b_lean_obj_arg br, size_t i) {\
-    return lean_pod_BytesRef_getUInt##bw##Ne(br, i);\
+LEAN_EXPORT lean_obj_res LEAN_POD_CONCAT3(lean_pod_BytesRef_getUInt, bw, LEAN_POD_BYTESREF_E_KEEP)(b_lean_obj_arg mut, size_t sz, b_lean_obj_arg br, size_t i) {\
+    return lean_pod_BytesRef_getUInt##bw##Ne(mut, sz, br, i);\
 }\
-LEAN_EXPORT lean_obj_res LEAN_POD_CONCAT3(lean_pod_BytesRef_getUInt, bw, LEAN_POD_BYTESREF_E_SWAP)(b_lean_obj_arg br, size_t i) {\
+LEAN_EXPORT lean_obj_res LEAN_POD_CONCAT3(lean_pod_BytesRef_getUInt, bw, LEAN_POD_BYTESREF_E_SWAP)(b_lean_obj_arg mut, size_t sz, b_lean_obj_arg br, size_t i) {\
     if (LEAN_LIKELY(lean_is_st(br))) {\
         uint##bw##_t u;\
         memcpy(&u, lean_pod_BytesView_unwrap(br)->ptr + i, sizeof(uint##bw##_t));\
@@ -44,17 +71,17 @@ LEAN_EXPORT lean_obj_res LEAN_POD_CONCAT3(lean_pod_BytesRef_getUInt, bw, LEAN_PO
     }\
     return lean_pod_BytesRef_notStError();\
 }\
-LEAN_EXPORT lean_obj_res lean_pod_BytesRef_setUInt##bw##Ne(b_lean_obj_arg br, size_t i, uint##bw##_t value) {\
+LEAN_EXPORT lean_obj_res lean_pod_BytesRef_setUInt##bw##Ne(size_t sz, b_lean_obj_arg br, size_t i, uint##bw##_t value) {\
     if (LEAN_LIKELY(lean_is_st(br))) {\
         memcpy(lean_pod_BytesView_unwrap(br)->ptr + i, &value, sizeof(uint##bw##_t));\
         return lean_io_result_mk_ok(lean_box(0));\
     }\
     return lean_pod_BytesRef_notStError();\
 }\
-LEAN_EXPORT lean_obj_res LEAN_POD_CONCAT3(lean_pod_BytesRef_setUInt, bw, LEAN_POD_BYTESREF_E_KEEP)(b_lean_obj_arg br, size_t i, uint##bw##_t value) {\
-    return lean_pod_BytesRef_setUInt##bw##Ne(br, i, value);\
+LEAN_EXPORT lean_obj_res LEAN_POD_CONCAT3(lean_pod_BytesRef_setUInt, bw, LEAN_POD_BYTESREF_E_KEEP)(size_t sz, b_lean_obj_arg br, size_t i, uint##bw##_t value) {\
+    return lean_pod_BytesRef_setUInt##bw##Ne(sz, br, i, value);\
 }\
-LEAN_EXPORT lean_obj_res LEAN_POD_CONCAT3(lean_pod_BytesRef_setUInt, bw, LEAN_POD_BYTESREF_E_SWAP)(b_lean_obj_arg br, size_t i, uint##bw##_t value) {\
+LEAN_EXPORT lean_obj_res LEAN_POD_CONCAT3(lean_pod_BytesRef_setUInt, bw, LEAN_POD_BYTESREF_E_SWAP)(size_t sz, b_lean_obj_arg br, size_t i, uint##bw##_t value) {\
     if (LEAN_LIKELY(lean_is_st(br))) {\
         uint##bw##_t value_bs = lean_pod_bswap##bw(value);\
         memcpy(lean_pod_BytesView_unwrap(br)->ptr + i, &value_bs, sizeof(uint##bw##_t));\
