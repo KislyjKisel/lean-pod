a4f6d2f4a3953465f7a01ad7e7d4b58413bf63a6
KislyjKisel <danilakisel@gmail.com>
2023-09-04 22:05:21 +0300

    Fix typos

--- src/lean/Pod/UInt.lean
+++ src/lean/Pod/UInt.lean
@@ -5,13 +5,13 @@ def UInt16.bswap (value : UInt16) : UInt16 :=
   (value <<< 8) ||| (value >>> 8)
 
 @[extern "lean_pod_UInt16_toBigEndian"]
-def UInt16.toBigEndianss (value : UInt16) :=
+def UInt16.toBigEndian (value : UInt16) :=
   if Pod.endianness = .big
     then value
     else value.bswap
 
 @[extern "lean_pod_UInt16_toLittleEndian"]
-def UInt16.toLittleEndianss (value : UInt16) :=
+def UInt16.toLittleEndian (value : UInt16) :=
   if Pod.endianness = .little
     then value
     else value.bswap
@@ -24,13 +24,13 @@ def UInt32.bswap (value : UInt32) : UInt32 :=
   ((value &&& 0xFF000000) >>> 24)
 
 @[extern "lean_pod_UInt32_toBigEndian"]
-def UInt32.toBigEndianss (value : UInt32) :=
+def UInt32.toBigEndian (value : UInt32) :=
   if Pod.endianness = .big
     then value
     else value.bswap
 
 @[extern "lean_pod_UInt32_toLittleEndian"]
-def UInt32.toLittleEndianss (value : UInt32) :=
+def UInt32.toLittleEndian (value : UInt32) :=
   if Pod.endianness = .little
     then value
     else value.bswap
@@ -47,13 +47,13 @@ def UInt64.bswap (value : UInt64) : UInt64 :=
   ((value &&& 0x00000000000000FF) <<< 56)
 
 @[extern "lean_pod_UInt64_toBigEndian"]
-def UInt64.toBigEndianss (value : UInt64) :=
+def UInt64.toBigEndian (value : UInt64) :=
   if Pod.endianness = .big
     then value
     else value.bswap
 
 @[extern "lean_pod_UInt64_toLittleEndian"]
-def UInt64.toLittleEndianss (value : UInt64) :=
+def UInt64.toLittleEndian (value : UInt64) :=
   if Pod.endianness = .little
     then value
     else value.bswap
@@ -62,13 +62,13 @@ def UInt64.toLittleEndianss (value : UInt64) :=
 opaque USize.bswap (value : USize) : USize
 
 @[extern "lean_pod_USize_toBigEndian"]
-def USize.toBigEndianss (value : USize) :=
+def USize.toBigEndian (value : USize) :=
   if Pod.endianness = .big
     then value
     else value.bswap
 
 @[extern "lean_pod_USize_toLittleEndian"]
-def USize.toLittleEndianss (value : USize) :=
+def USize.toLittleEndian (value : USize) :=
   if Pod.endianness = .little
     then value
     else value.bswap
