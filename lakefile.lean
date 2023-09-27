804b9763067d10cfa1927ce40a74627b564e51c0
KislyjKisel <danilakisel@gmail.com>
2023-09-27 13:26:56 +0300

    Toolchain `4.1.0`

--- lakefile.lean
+++ lakefile.lean
@@ -10,33 +10,38 @@ lean_lib Pod
 @[default_target]
 lean_exe Main
 
-def buildBindingsO (pkg : NPackage _package.name) (flags : Array String) (stem : String) : IndexBuildM (BuildJob FilePath) := do
-  let oFile := pkg.irDir / "native" / (stem ++ ".o")
-  let srcJob ← inputFile <| pkg.dir / "src" / "native" / (stem ++ ".c")
-  buildO (stem ++ ".c") oFile srcJob flags ((get_config? cc).getD (← getLeanCc).toString)
+def buildBindingsO
+  (pkg : NPackage _package.name) (weakArgs traceArgs : Array String) (stem : String) :
+    IndexBuildM (BuildJob FilePath) := do
+      let oFile := pkg.irDir / "native" / (stem ++ ".o")
+      let srcJob ← inputFile <| pkg.dir / "src" / "native" / (stem ++ ".c")
+      buildO (stem ++ ".c") oFile srcJob weakArgs traceArgs
+        ((get_config? cc).getD (← getLeanCc).toString)
 
-extern_lib «lean-pod» (pkg : NPackage _package.name) := do
+extern_lib «lean-pod» pkg := do
   let name := nameToStaticLib "lean-pod"
-  let mut flags :=
-    #["-I", (← getLeanIncludeDir).toString, "-fPIC"].append $
+  let mut weakArgs :=
+    #["-I", (← getLeanIncludeDir).toString].append $
       Array.mk $ ((get_config? cflags).getD "").splitOn.filter $ not ∘ String.isEmpty
+
+  let mut traceArgs := #["-fPIC"]
 
   match get_config? alloc with
   | .none | .some "lean" => pure ()
-  | .some "native" => flags := flags.push "-DLEAN_POD_ALLOC_NATIVE"
+  | .some "native" => traceArgs := traceArgs.push "-DLEAN_POD_ALLOC_NATIVE"
   | .some _ => error "Unknown `alloc` option"
 
   if (get_config? cc).isNone then
-    flags := flags ++ #["-I", ((← getLeanIncludeDir) / "clang").toString]
+    weakArgs := weakArgs ++ #["-I", ((← getLeanIncludeDir) / "clang").toString]
 
   buildStaticLib (pkg.nativeLibDir / name) #[
-    (← buildBindingsO pkg flags "endianness"),
-    (← buildBindingsO pkg flags "storable"),
-    (← buildBindingsO pkg flags "uint"),
-    (← buildBindingsO pkg flags "float"),
-    (← buildBindingsO pkg flags "bytes_view"),
-    (← buildBindingsO pkg flags "bytes_ref"),
-    (← buildBindingsO pkg flags "buffer"),
-    (← buildBindingsO pkg flags "instances"),
-    (← buildBindingsO pkg flags "uvector")
+    (← buildBindingsO pkg weakArgs traceArgs "endianness"),
+    (← buildBindingsO pkg weakArgs traceArgs "storable"),
+    (← buildBindingsO pkg weakArgs traceArgs "uint"),
+    (← buildBindingsO pkg weakArgs traceArgs "float"),
+    (← buildBindingsO pkg weakArgs traceArgs "bytes_view"),
+    (← buildBindingsO pkg weakArgs traceArgs "bytes_ref"),
+    (← buildBindingsO pkg weakArgs traceArgs "buffer"),
+    (← buildBindingsO pkg weakArgs traceArgs "instances"),
+    (← buildBindingsO pkg weakArgs traceArgs "uvector")
   ]
