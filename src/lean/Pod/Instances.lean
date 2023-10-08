1d6d97070e2a4c0f7ab0ba9df2cec2e82c3cdb0c
KislyjKisel <danilakisel@gmail.com>
2023-10-08 19:26:00 +0300

    Expose meta means to gen rwbytes instances

--- src/lean/Pod/Instances.lean
+++ src/lean/Pod/Instances.lean
@@ -1,245 +1,179 @@
-import Lean.Elab.Command
-import Pod.Lemmas
+import Lean.Syntax
 import Pod.Float
 import Pod.Storable
 import Pod.ReadBytes
 import Pod.WriteBytes
 
 open Lean Syntax
-open Lean Elab Command
 
 namespace Pod
 
 section Instances
 
-set_option maxRecDepth 1024
-
-scoped elab "def_rwbytes_inst" typeName:str : command => do
+macro "#pod_c_rwbytes_instance" typeName:str : command => do
   let typeStr := typeName.getString
   let typeId := mkIdent $ Name.mkSimple typeStr
+  let declName_rra0 := "readBytesRef_" ++ typeStr
+  let declName_rra1 := "readBytesRefOff_" ++ typeStr
+  let declName_rra2 := "readBytesRefOffEl_" ++ typeStr
+  let declName_rru0 := "readBytesRefUnal_" ++ typeStr
+  let declName_rru1 := "readBytesRefOffUnal_" ++ typeStr
+  let declName_rru2 := "readBytesRefOffElUnal_" ++ typeStr
+  let declName_va0 := "readBytesView_" ++ typeStr
+  let declName_va1 := "readBytesViewOff_" ++ typeStr
+  let declName_va2 := "readBytesViewOffEl_" ++ typeStr
+  let declName_vu0 := "readBytesViewUnal_" ++ typeStr
+  let declName_vu1 := "readBytesViewOffUnal_" ++ typeStr
+  let declName_vu2 := "readBytesViewOffElUnal_" ++ typeStr
+  let declName_rwa0 := "writeBytesRef_" ++ typeStr
+  let declName_rwa1 := "writeBytesRefOff_" ++ typeStr
+  let declName_rwa2 := "writeBytesRefOffEl_" ++ typeStr
+  let declName_rwu0 := "writeBytesRefUnal_" ++ typeStr
+  let declName_rwu1 := "writeBytesRefOffUnal_" ++ typeStr
+  let declName_rwu2 := "writeBytesRefOffElUnal_" ++ typeStr
+  pure $ TSyntax.mk $ mkNullNode #[
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rra0):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rra0)
+        {σ} (br : @& BytesRefImm σ (byteSize $typeId) (alignment $typeId)) : ST σ $typeId
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rra1):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rra1)
+        {σ} {size align : @& Nat} (br : @& BytesRefImm σ size align) (i : @& Nat)
+        (h₁ : i + (byteSize $typeId) ≤ size)
+        (h₂ : ∀ m, ∃ n, align * m + i = (alignment $typeId) * n) : ST σ $typeId
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rra2):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rra2)
+        {σ} {size : @& Nat} (br : @& BytesRefImm σ size (alignment $typeId)) (i : @& Nat)
+        (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) : ST σ $typeId
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rru0):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rru0)
+        {σ} {align : @& Nat} (br : @& BytesRefImm σ (byteSize $typeId) align) : ST σ $typeId
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rru1):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rru1)
+        {σ} {size align : @& Nat} (br : @& BytesRefImm σ size align) (i : @& Nat)
+        (h₁ : i + (byteSize $typeId) ≤ size) : ST σ $typeId
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rru2):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rru2)
+        {σ} {size align : @& Nat} (br : @& BytesRefImm σ size align) (i : @& Nat)
+        (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) : ST σ $typeId
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_va0):str]
+      opaque $(mkIdent $ Name.mkSimple declName_va0)
+        (bv : @& BytesView (byteSize $typeId) (alignment $typeId)) : $typeId
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_va1):str]
+      opaque $(mkIdent $ Name.mkSimple declName_va1)
+        {size align : @& Nat} (bv : @& BytesView size align) (i : @& Nat)
+        (h₁ : i + (byteSize $typeId) ≤ size)
+        (h₂ : ∀ m, ∃ n, align * m + i = (alignment $typeId) * n) : $typeId
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_va2):str]
+      opaque $(mkIdent $ Name.mkSimple declName_va2)
+        {size : @& Nat} (bv : @& BytesView size (alignment $typeId)) (i : @& Nat)
+        (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) : $typeId
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_vu0):str]
+      opaque $(mkIdent $ Name.mkSimple declName_vu0)
+         {align : @& Nat} (bv : @& BytesView (byteSize $typeId) align) : $typeId
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_vu1):str]
+      opaque $(mkIdent $ Name.mkSimple declName_vu1)
+        {size align : @& Nat} (bv : @& BytesView size align) (i : @& Nat)
+        (h₁ : i + (byteSize $typeId) ≤ size) : $typeId
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_vu2):str]
+      opaque $(mkIdent $ Name.mkSimple declName_vu2)
+        {size align : @& Nat} (bv : @& BytesView size align) (i : @& Nat)
+        (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) : $typeId
+    ),
+    ← `(command|
+      instance : ReadBytes $typeId where
+        readBytesRef := $(mkIdent declName_rra0)
+        readBytesRefOff := $(mkIdent declName_rra1)
+        readBytesRefOffEl := $(mkIdent declName_rra2)
+        readBytesRefUnal := $(mkIdent declName_rru0)
+        readBytesRefOffUnal := $(mkIdent declName_rru1)
+        readBytesRefOffElUnal := $(mkIdent declName_rru2)
+        readBytesView := $(mkIdent declName_va0)
+        readBytesViewOff := $(mkIdent declName_va1)
+        readBytesViewOffEl := $(mkIdent declName_va2)
+        readBytesViewUnal := $(mkIdent declName_vu0)
+        readBytesViewOffUnal := $(mkIdent declName_vu1)
+        readBytesViewOffElUnal := $(mkIdent declName_vu2)
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rwa0):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rwa0)
+        {σ} (br : @& BytesRefMut σ (byteSize $typeId) (alignment $typeId))
+        (value : $typeId) : ST σ Unit
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rwa1):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rwa1)
+        {σ} {size align : @& Nat}
+        (br : @& BytesRefMut σ size align) (i : @& Nat) (value : $typeId)
+        (h₁ : i + (byteSize $typeId) ≤ size)
+        (h₂ : ∀ m, ∃ n, align * m + i = (alignment $typeId) * n) :
+        ST σ Unit
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rwa2):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rwa2)
+        {σ} {size : @& Nat} (br : @& BytesRefMut σ size (alignment $typeId)) (i : @& Nat)
+        (value : $typeId) (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) : ST σ Unit
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rwu0):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rwu0)
+        {σ} {align : @& Nat} (br : @& BytesRefMut σ (byteSize $typeId) align)
+        (value : $typeId) : ST σ Unit
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rwu1):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rwu1)
+        {σ} {size align : @& Nat} (br : @& BytesRefMut σ size align) (i : @& Nat)
+        (value : $typeId) (h₁ : i + (byteSize $typeId) ≤ size) : ST σ Unit
+    ),
+    ← `(command|
+      @[extern $(mkStrLit $ "lean_pod_" ++ declName_rwu2):str]
+      opaque $(mkIdent $ Name.mkSimple declName_rwu2)
+        {σ} {size align : @& Nat} (br : @& BytesRefMut σ size align) (i : @& Nat) (value : $typeId)
+        (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) : ST σ Unit
+    ),
+    ← `(command|
+      instance : WriteBytes $typeId where
+        writeBytesRef := $(mkIdent declName_rwa0)
+        writeBytesRefOff := $(mkIdent declName_rwa1)
+        writeBytesRefOffEl := $(mkIdent declName_rwa2)
+        writeBytesRefUnal := $(mkIdent declName_rwu0)
+        writeBytesRefOffUnal := $(mkIdent declName_rwu1)
+        writeBytesRefOffElUnal := $(mkIdent declName_rwu2)
+    )
+  ]
 
-  let declStr := "readBytesRef_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rra0 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rra0 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ} (br : @& BytesRefImm σ (byteSize $typeId) (alignment $typeId)) : ST σ $typeId
-  )
-
-  let declStr := "readBytesRefOff_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rra1 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rra1 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ} {size align : @& Nat} (br : @& BytesRefImm σ size align) (i : @& Nat)
-      (h₁ : i + (byteSize $typeId) ≤ size)
-      (h₂ : ∀ m, ∃ n, align * m + i = (alignment $typeId) * n) : ST σ $typeId
-  )
-
-  let declStr := "readBytesRefOffEl_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rra2 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rra2 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ} {size : @& Nat} (br : @& BytesRefImm σ size (alignment $typeId)) (i : @& Nat)
-      (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) : ST σ $typeId
-  )
-
-  let declStr := "readBytesRefUnal_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rru0 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rru0 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ} {align : @& Nat} (br : @& BytesRefImm σ (byteSize $typeId) align) : ST σ $typeId
-  )
-
-  let declStr := "readBytesRefOffUnal_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rru1 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rru1 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ} {size align : @& Nat} (br : @& BytesRefImm σ size align) (i : @& Nat)
-      (h₁ : i + (byteSize $typeId) ≤ size) : ST σ $typeId
-  )
-
-  let declStr := "readBytesRefOffElUnal_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rru2 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rru2 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ} {size align : @& Nat} (br : @& BytesRefImm σ size align) (i : @& Nat)
-      (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) : ST σ $typeId
-  )
-
-  let declStr := "readBytesView_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_va0 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_va0 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) (bv : @& BytesView (byteSize $typeId) (alignment $typeId)) : $typeId
-  )
-
-  let declStr := "readBytesViewOff_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_va1 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_va1 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {size align : @& Nat} (bv : @& BytesView size align) (i : @& Nat)
-      (h₁ : i + (byteSize $typeId) ≤ size)
-      (h₂ : ∀ m, ∃ n, align * m + i = (alignment $typeId) * n) : $typeId
-  )
-
-  let declStr := "readBytesViewOffEl_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_va2 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_va2 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {size : @& Nat} (bv : @& BytesView size (alignment $typeId)) (i : @& Nat)
-      (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) : $typeId
-  )
-
-  let declStr := "readBytesViewUnal_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_vu0 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_vu0 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {align : @& Nat} (bv : @& BytesView (byteSize $typeId) align) : $typeId
-  )
-
-  let declStr := "readBytesViewOffUnal_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_vu1 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_vu1 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {size align : @& Nat} (bv : @& BytesView size align) (i : @& Nat)
-      (h₁ : i + (byteSize $typeId) ≤ size) : $typeId
-  )
-
-  let declStr := "readBytesViewOffElUnal_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_vu2 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_vu2 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {size align : @& Nat} (bv : @& BytesView size align) (i : @& Nat)
-      (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) : $typeId
-  )
-
-  let declStr := "writeBytesRef_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rwa0 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rwa0 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ}
-      (br : @& BytesRefMut σ (byteSize $typeId) (alignment $typeId)) (value : $typeId) :
-      ST σ Unit
-  )
-
-  let declStr := "writeBytesRefOff_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rwa1 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rwa1 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ} {size align : @& Nat}
-      (br : @& BytesRefMut σ size align) (i : @& Nat) (value : $typeId)
-      (h₁ : i + (byteSize $typeId) ≤ size)
-      (h₂ : ∀ m, ∃ n, align * m + i = (alignment $typeId) * n) :
-      ST σ Unit
-  )
-
-  let declStr := "writeBytesRefOffEl_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rwa2 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rwa2 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ} {size : @& Nat}
-      (br : @& BytesRefMut σ size (alignment $typeId)) (i : @& Nat) (value : $typeId)
-      (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) :
-      ST σ Unit
-  )
-
-  let declStr := "writeBytesRefUnal_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rwu0 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rwu0 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ} {align : @& Nat}
-      (br : @& BytesRefMut σ (byteSize $typeId) align) (value : $typeId) :
-      ST σ Unit
-  )
-
-  let declStr := "writeBytesRefOffUnal_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rwu1 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rwu1 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ} {size align : @& Nat}
-      (br : @& BytesRefMut σ size align) (i : @& Nat) (value : $typeId)
-      (h₁ : i + (byteSize $typeId) ≤ size) :
-      ST σ Unit
-  )
-
-  let declStr := "writeBytesRefOffElUnal_" ++ typeStr
-  let declName := Name.mkSimple $ declStr
-  let declName_rwu2 := `Pod ++ declName
-  setEnv $ ← ofExcept $ externAttr.setParam (← getEnv) declName_rwu2 $
-    ExternAttrData.mk none $ [ExternEntry.standard `all s!"lean_pod_{declStr}"]
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    opaque $(mkIdent declName) {σ} {size align : @& Nat}
-      (br : @& BytesRefMut σ size align) (i : @& Nat) (value : $typeId)
-      (h : i * (byteSize $typeId) + (byteSize $typeId) ≤ size) :
-      ST σ Unit
-  )
-
-
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    instance : ReadBytes $typeId where
-      readBytesRefOffUnal := $(mkIdent declName_rru1)
-      readBytesRefUnal := $(mkIdent declName_rru0)
-      readBytesRefOffElUnal := $(mkIdent declName_rru2)
-      readBytesRefOff := $(mkIdent declName_rra1)
-      readBytesRef := $(mkIdent declName_rra0)
-      readBytesRefOffEl := $(mkIdent declName_rra2)
-      readBytesViewOffUnal := $(mkIdent declName_vu1)
-      readBytesViewUnal := $(mkIdent declName_vu0)
-      readBytesViewOffElUnal := $(mkIdent declName_vu2)
-      readBytesViewOff := $(mkIdent declName_va1)
-      readBytesView := $(mkIdent declName_va0)
-      readBytesViewOffEl := $(mkIdent declName_va2)
-  )
-  Lean.Elab.Command.elabCommand $ ← `(command|
-    instance : WriteBytes $typeId where
-      writeBytesRefOffUnal := $(mkIdent declName_rwu1)
-      writeBytesRefUnal := $(mkIdent declName_rwu0)
-      writeBytesRefOffElUnal := $(mkIdent declName_rwu2)
-      writeBytesRefOff := $(mkIdent declName_rwa1)
-      writeBytesRef := $(mkIdent declName_rwa0)
-      writeBytesRefOffEl := $(mkIdent declName_rwa2)
-  )
-
-def_rwbytes_inst "UInt8"
-def_rwbytes_inst "UInt16"
-def_rwbytes_inst "UInt32"
-def_rwbytes_inst "UInt64"
-def_rwbytes_inst "USize"
-def_rwbytes_inst "Float"
-def_rwbytes_inst "Float32"
+#pod_c_rwbytes_instance "UInt8"
+#pod_c_rwbytes_instance "UInt16"
+#pod_c_rwbytes_instance "UInt32"
+#pod_c_rwbytes_instance "UInt64"
+#pod_c_rwbytes_instance "USize"
+#pod_c_rwbytes_instance "Float"
+#pod_c_rwbytes_instance "Float32"
 
 instance {size alignment} : GetElem (Pod.BytesView size alignment) Nat UInt8 λ _ i ↦ i < size where
   getElem bv i h := Pod.readBytesViewOff_UInt8 bv i h
