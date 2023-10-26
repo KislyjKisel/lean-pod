96196a4d2f1f65cdb56f8fdd7bfc0f76acb75c5c
KislyjKisel <danilakisel@gmail.com>
2023-10-27 00:09:54 +0300

    Signed integers `OfNat`

--- src/lean/Pod/Int.lean
+++ src/lean/Pod/Int.lean
@@ -100,6 +100,7 @@ instance : Neg Int8 where
 instance : Mul Int8 := ⟨Int8.mul⟩
 instance : Div Int8 := ⟨Int8.div⟩
 instance : Mod Int8 := ⟨Int8.mod⟩
+instance {n} : OfNat Int8 n := ⟨Int8.ofUInt8 $ UInt8.ofNat n⟩
 instance : ToString Int8 where
   toString x := ite x.sign s!"-{toString (-x).val}" (toString x.val)
 instance : BEq Int8 where
@@ -223,6 +224,7 @@ instance : Neg Int16 where
 instance : Mul Int16 := ⟨Int16.mul⟩
 instance : Div Int16 := ⟨Int16.div⟩
 instance : Mod Int16 := ⟨Int16.mod⟩
+instance {n} : OfNat Int16 n := ⟨Int16.ofUInt16 $ UInt16.ofNat n⟩
 instance : ToString Int16 where
   toString x := ite x.sign s!"-{toString (-x).val}" (toString x.val)
 instance : BEq Int16 where
@@ -346,6 +348,7 @@ instance : Neg Int32 where
 instance : Mul Int32 := ⟨Int32.mul⟩
 instance : Div Int32 := ⟨Int32.div⟩
 instance : Mod Int32 := ⟨Int32.mod⟩
+instance {n} : OfNat Int32 n := ⟨Int32.ofUInt32 $ UInt32.ofNat n⟩
 instance : ToString Int32 where
   toString x := ite x.sign s!"-{toString (-x).val}" (toString x.val)
 instance : BEq Int32 where
@@ -469,6 +472,7 @@ instance : Neg Int64 where
 instance : Mul Int64 := ⟨Int64.mul⟩
 instance : Div Int64 := ⟨Int64.div⟩
 instance : Mod Int64 := ⟨Int64.mod⟩
+instance {n} : OfNat Int64 n := ⟨Int64.ofUInt64 $ UInt64.ofNat n⟩
 instance : ToString Int64 where
   toString x := ite x.sign s!"-{toString (-x).val}" (toString x.val)
 instance : BEq Int64 where
