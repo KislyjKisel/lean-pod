9701a7f54fa4d2fd3a891abde7caf76e7b6f7146
KislyjKisel <danilakisel@gmail.com>
2024-06-23 04:42:45 +0300

    Allow binders in `define_foreign_type`

--- src/lean/Pod/Meta.lean
+++ src/lean/Pod/Meta.lean
@@ -35,16 +35,31 @@ macro doc:(docComment)? "define_foreign_constant" name:ident &" : " type:term &"
     )
 
 /-- Defines a type that can be used to pass opaque foreign objects. -/
-macro doc:(docComment)? "define_foreign_type" name:ident : command =>
+macro doc:(docComment)? "define_foreign_type" name:ident binders:bracketedBinder* : command => do
   let pairIdent := Lean.mkIdent (Lean.Name.appendAfter name.getId "Pointed")
+  let argNames : Array Lean.Syntax := binders.filterMap λ x ↦
+    match x.raw with
+    | Lean.Syntax.node _ `Lean.Parser.Term.explicitBinder args => do
+      let name := (← args.get? 1).getArg 0
+      if name.isMissing
+        then none
+        else some name
+    | _ => none
+  let applied := Lean.TSyntaxArray.mk argNames
+  -- let instanceArgs : Lean.TSyntaxArray `Lean.Parser.Term.implicitBinder :=
+  --   Lean.TSyntaxArray.mk $ argNames.map λ n ↦
+  --     dbg_trace n;
+  --     Lean.Syntax.node .none `Lean.Parser.Term.implicitBinder #[.atom .none "{", n, .atom .none "}"]
   if let some doc := doc
     then `(
-      private opaque $pairIdent : NonemptyType
-      $doc:docComment def $name : Type := $(pairIdent).type
-      instance : Nonempty $name := $(pairIdent).property
+      private opaque $pairIdent $binders* : NonemptyType
+      $doc:docComment def $name $binders* : Type := ($(pairIdent) $applied*).type
+      set_option autoImplicit true in
+      instance : Nonempty ($name $applied*) := ($(pairIdent) $applied*).property
     )
     else `(
-      private opaque $pairIdent : NonemptyType
-      def $name : Type := $(pairIdent).type
-      instance : Nonempty $name := $(pairIdent).property
+      private opaque $pairIdent $binders* : NonemptyType
+      def $name $binders* : Type := ($(pairIdent) $applied*).type
+      set_option autoImplicit true in
+      instance : Nonempty ($name $applied*) := ($(pairIdent) $applied*).property
     )
