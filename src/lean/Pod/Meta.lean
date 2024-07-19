c4bd5965b1ad03882dcb1db23007a848fb2d0245
KislyjKisel <danilakisel@gmail.com>
2024-07-19 16:35:32 +0300

    Meta: refactor, add `static_assert`

--- src/lean/Pod/Meta.lean
+++ src/lean/Pod/Meta.lean
@@ -3,21 +3,23 @@ import Lean.Parser.Command
 namespace Pod
 
 /-- Defines a constant whose value is defined externally in foreign code. -/
-scoped macro mods:declModifiers "define_foreign_constant" name:ident &" : " type:term &" := " ext:str : command => do
+scoped macro mods:declModifiers "define_foreign_constant " name:ident &" : " type:term &" := " ext:str : command => do
   `(@[extern $ext:str] private
     opaque getValue : @& Unit → $type
     $mods:declModifiers
     def $name : $type := getValue ())
 
 /-- Defines a constant whose value is defined using foreign code. -/
-scoped macro mods:declModifiers "define_foreign_constant" name:ident &" : " type:term &" := " &" inline " expr:str : command => do
+scoped macro mods:declModifiers "define_foreign_constant " name:ident &" : " type:term &" := " &" inline " expr:str : command => do
   `(@[extern c inline $expr:str] private
     opaque getValue : Unit → $type
     $mods:declModifiers
     def $name : $type := getValue ())
 
+syntax foreignTypeSort := &" : " &"Type " level
+
 /-- Defines a type that can be used to pass opaque foreign objects. -/
-scoped macro mods:declModifiers "define_foreign_type" id:declId binders:bracketedBinder* : command => do
+scoped macro mods:declModifiers "define_foreign_type " id:declId binders:bracketedBinder* ty:(foreignTypeSort)? : command => do
   let name := Lean.TSyntax.mk (id.raw.getArg 0)
   let univs := (id.raw.getArg 1).getArgs
   let univs := univs[1:univs.size - 1]
@@ -30,17 +32,28 @@ scoped macro mods:declModifiers "define_foreign_type" id:declId binders:brackete
       Lean.Syntax.getArgs <$> (args.get? 1) |>.getD #[]
     | _ => #[]
   let applied := Lean.TSyntaxArray.mk argNames
+  let level : Lean.TSyntax `level := ty.elim
+    (Lean.TSyntax.mk <| Lean.Syntax.mkNumLit "0")
+    λ x ↦ Lean.TSyntax.mk (x.raw.getArg 2)
   -- let instanceArgs : Lean.TSyntaxArray `Lean.Parser.Term.implicitBinder :=
   --   Lean.TSyntaxArray.mk $ argNames.map λ n ↦
   --     dbg_trace n;
   --     Lean.Syntax.node .none `Lean.Parser.Term.implicitBinder #[.atom .none "{", n, .atom .none "}"]
-  `(private opaque $pairIdent.{$univs,*} $binders* : NonemptyType
+  `(private opaque $pairIdent.{$univs,*} $binders* : NonemptyType.{$level}
     $mods:declModifiers
-    def $name.{$univs,*} $binders* : Type := ($(pairIdent) $applied*).type
+    def $name.{$univs,*} $binders* : Type $level := ($(pairIdent) $applied*).type
     set_option autoImplicit true in
     instance : Nonempty ($name $applied*) := ($(pairIdent) $applied*).property)
 
 scoped macro "extern_initialize" &" => " cFn:str : command =>
   `(@[extern $cFn:str] private
     opaque initializeFn : BaseIO Unit
-    builtin_initialize initializeFn)
+    initialize initializeFn)
+
+scoped macro "static_assert " name:ident &" : " type:term : command =>
+  let errorMessage : Lean.TSyntax `term :=
+    Lean.TSyntax.mk $ Lean.Syntax.mkStrLit s!"`static_assert` {name} failed"
+  `(
+    initialize if $type then pure () else throw (IO.userError $errorMessage)
+    axiom $name : $type
+  )
