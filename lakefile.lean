6552d3c4804f6fdfe6b06008164791cf1af04ff4
KislyjKisel <danilakisel@gmail.com>
2025-04-27 21:51:23 +0300

    Update toolchain to nightly

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ def optionAllocator := get_config? alloc
 def optionPrecompile := (get_config? precompile).isSome
 
 require LSpec from git
-  "https://github.com/argumentcomputer/LSpec" @ "a6652a4"
+  "https://github.com/argumentcomputer/LSpec" @ "24cceb6"
 
 package «pod» where
   srcDir := "src"
