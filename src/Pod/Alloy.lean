87a1e511959d11785374a2bac7cd616717343977
KislyjKisel <danilakisel@gmail.com>
2026-07-09 19:16:01 +0300

    add `alloy print` elab macro

--- src/Pod/Alloy.lean
+++ src/Pod/Alloy.lean
@@ -8,6 +8,9 @@ public section
 
 namespace Pod
 
+scoped elab "alloy " &"c " &"print" : command => do
+  println! Alloy.C.shimExt.getState (← Lean.getEnv) |>.text.source
+
 /--
 `define_foreign_constant my_const : Float := alloy c { return 2.4; }`
 creates a parameterless function with the specified body using Alloy.
